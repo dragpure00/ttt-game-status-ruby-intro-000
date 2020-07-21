3b2655cc40dce727781e8ed38472dd7866c0780e
Ayaz Araf <ayaz.araf@gmail.com>
2020-07-21 22:36:02 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,21 +20,42 @@ WIN_COMBINATIONS = [
 ]
 board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
 
-def won?(board)
-  winner=[]
-  emptyb=board.all?  {|x| x == " "}
-  WIN_COMBINATIONS.each do |sub_array|
-    if emptyb || full?(board)
-      return false
-    elsif sub_array.all? { |value| board[value] =="X" } || sub_array.all? { |value| board[value] =="O" }
-      winner = sub_array
+def winner(board)
+  index=[]
+  index=won?(board)
+  if index==false
+    return nil
+  else
+    if index[0]=="X"
+      return "X"
+    else
+      return "O"
     end
   end
-  winner
+end
+  
+def won?(board)
+  WIN_COMBINATIONS.each|wc|
+  win_index_1=WIN_COMBINATIONS[0]
+  win_index_2=WIN_COMBINATIONS[1]
+  win_index_3=WIN_COMBINATIONS[2]
+
+p1=board[win_index_1]
+p2=board[win_index_2]
+p3=board[win_index_3]
+
+if position_1== "X"&&position_2=="X"&&position_3=="X"
+  return wc
+else
+  position_1== "O"&&position_2=="O"&&position_3=="O"
+  return wc
+end
+return false
 end
 
 def full?(board)
-  !board.any? { |x| x == " " }
+  !board.any? { |index| index == "X "||index=="O" }
+  
 end
 
 def draw?(board)
