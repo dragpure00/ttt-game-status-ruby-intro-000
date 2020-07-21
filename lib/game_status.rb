244148acd5d6e2c59301aa3191aa154777b7526c
Ayaz Araf <ayaz.araf@gmail.com>
2020-07-21 22:49:47 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,68 +1,75 @@
+# Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [6, 4, 2]
+[0,1,2],
+[3,4,5],
+[6,7,8],
+
+[0,3,6],
+[1,4,7],
+[2,5,8],
+
+[0,4,8],
+[6,4,2]
+
+
 ]
 
-def won?(board)
-  WIN_COMBINATIONS.each {|win_combo|
-    index_0 = win_combo[0]
-    index_1 = win_combo[1]
-    index_2 = win_combo[2]
-
-    position_1 = board[index_0]
-    position_2 = board[index_1]
-    position_3 = board[index_2]
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combo
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combo
-    end
-  }
-  return false
-end
-
-def full?(board)
-  board.all? {|index| index == "X" || index == "O"}
-end
-
-def draw?(board)
-  if !won?(board) && full?(board)
-    return true
-  else
-    return false
-  end
-end
-
-def over?(board)
-  if won?(board) || full?(board) || draw?(board)
-    return true
-  else
-    return false
-  end
-end
-
-def winner (board)
-  index = []
-  index = won?(board)
-  if index == false
+def winner(board)
+  index=[]
+  index=won?(board)
+  if index==false
     return nil
   else
-    if board[index[0]] == "X"
+    if index[0]=="X"
       return "X"
     else
       return "O"
     end
   end
 end
+
+def won?(board)
+  WIN_COMBINATIONS.each{|wc|
+  win_index_1=wc[0]
+  win_index_2=wc[1]
+  win_index_3=wc[2]
+
+p1=board[win_index_1]
+p2=board[win_index_2]
+p3=board[win_index_3]
+
+if p1== "X"&&p2=="X"&&p3=="X"
+  return wc
+else
+  p1== "O"&&p2=="O"&&p3=="O"
+  return wc
+end
+}
+return false
+end
+
+def full?(board)
+  !board.any? { |index| index == "X "||index=="O" }
+
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  elseif !won?(board) && full?(board)|| !won?(board)
+    return false
+  end
+end
+def over?(board)
+  if(won?(board)||full?(board)||draw?(board))
+    return ture
+  else
+    return false
+  end
+end
+draw?(board)
