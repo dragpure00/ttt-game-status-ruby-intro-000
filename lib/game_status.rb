c838e9dbdf99ae56430390d5e51587c8fb5eae0c
Ayaz Araf <ayaz.araf@gmail.com>
2020-07-21 23:42:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,6 +19,19 @@ WIN_COMBINATIONS = [
 
 ]
 
+def winner(board)
+  index=[]
+  index=won?(board)
+  if index==false
+    return nil
+  else
+    if board[index[0]]== "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
 
 
 def won?(board)
@@ -62,19 +75,3 @@ def over?(board)
   end
 end
 
-
-
-
-def winner(board)
-  index=[]
-  index=won?(board)
-  if index==false
-    return nil
-  else
-    if board[index[0]]== "X"
-      return "X"
-    else
-      return "O"
-    end
-  end
-end
